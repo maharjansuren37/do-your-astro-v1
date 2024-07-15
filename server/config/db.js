81054a1305dd4e93d1cd41412fa85538cf29c6c0
maharjansuren37 <maharjansuren37@gmail.com>
2024-07-16 08:50:16 +0930

    updated files

--- server/config/db.js
+++ server/config/db.js
@@ -1,13 +1,15 @@
+const dotenv = require('dotenv').config({ path: '../../.env'});
 const mongoose = require('mongoose');
 
-const connectDB = async () => {
-    try {
-        const connect = await mongoose.connect(process.env.MONGODB_URI);
-        console.log(`Mongo DB Connected: ${connect.connection.host}`);
-    } catch(err) {
-        console.log(err);
-        process.exit(1);
-    }
-}
+
+// const connectDB = async () => {
+//     try {
+//         const connect = await mongoose.connect(process.env.MONGODB_URI);
+//         console.log(`Mongo DB Connected: ${connect.connection.host}`);
+//     } catch(err) {
+//         console.log(err);
+//         process.exit(1);
+//     }
+// }
 
 module.exports = connectDB;
