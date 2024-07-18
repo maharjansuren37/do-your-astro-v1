ff69ec681023646ad28cf9f6e10488130103452b
maharjansuren37 <maharjansuren37@gmail.com>
2024-07-18 19:12:59 +0930

    updated home model

--- server/models/homeModel.js
+++ server/models/homeModel.js
@@ -1,17 +1,25 @@
 const mongoose = require('mongoose');
 
 const homeSchema = mongoose.Schema({
-    title: {
-        type: String,
-        required: [false]
+    home: {
+        title: {
+            type: String,
+            required: [false]
+        },
+        subtitle: {
+            type: String,
+            required: [false]
+        },
+        img: {
+            type: String,
+            required: [false]
+        }
     },
-    subtitle: {
-        type: String,
-        required: [false]
-    },
-    img: {
-        type: String,
-        required: [false]
+    astroSection: {
+        
+    } ,
+    bookbinderySection: {
+
     }
 }, {
     timestamps: true,
