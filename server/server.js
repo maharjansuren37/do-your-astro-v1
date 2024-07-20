55e499dc708eb39536e64cb702fd37a5bb2a1b58
maharjansuren37 <maharjansuren37@gmail.com>
2024-07-20 16:30:37 +0930

    added more routes

--- server/server.js
+++ server/server.js
@@ -1,9 +1,8 @@
 const path = require('path');
 const express = require('express');
-
 const dotenv = require('dotenv').config();
-const { errorHandler } = require('./middleware/errorMiddleware');
 
+const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
 
 const port = process.env.PORT || 5000;
@@ -19,6 +18,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/api/goals', require('./routes/goalRoutes'));
 
 app.use('/api/home', require('./routes/homeRoutes'));
+
+app.use('/api/astro', require('/routes/astroRoutes'));
+
+app.use('/api/bookbindery',require('./routes/bookRoutes'));
 
 app.use(errorHandler);
 
