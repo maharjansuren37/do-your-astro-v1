83f5caf3bccc28d18e5abec05e917012609a944f
maharjansuren37 <maharjansuren37@gmail.com>
2024-07-25 07:01:44 +0930

    add new routes

--- client/src/App.js
+++ client/src/App.js
@@ -5,13 +5,29 @@ import {
   RouterProvider 
 } from "react-router-dom";
 import RootLayout from "./layouts/RootLayout";
+import Admin from "./pages/Admin";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<RootLayout />}>
-
-    </Route>
+    <>
+      <Route path="/" element={<RootLayout />}>
+      
+      </Route>
+      <Route path="/admin" element={<Admin />}>
+  
+      </Route>
+    </> 
   )
+  // [
+  //   {
+  //     path: '/',
+  //     element: <RootLayout />
+  //   },
+  //   {
+  //     path: '/admin',
+  //     element: <Admin />
+  //   }
+  // ]
 )
 
 function App() {
