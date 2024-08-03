291a870555580090b8c1bd1bd6e847dc4690c005
maharjansuren37 <maharjansuren37@gmail.com>
2024-08-03 13:49:19 +0930

    updated edit homepage form

--- client/src/layouts/Admin/AdminHeader.js
+++ client/src/layouts/Admin/AdminHeader.js
@@ -1,4 +1,5 @@
 // import { FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons';
+import { NavLink } from 'react-router-dom';
 import { useAuth } from '../../hooks/AuthProvider';
 
 export default function AdminHeader() {
@@ -8,7 +9,17 @@ export default function AdminHeader() {
         <header className='admin-header'>
             <div className='container flex-d align-center justify-space-between'>
                 <span className='logo'>Do Your Astro</span>
-                {auth.token && <button className='btn' onClick={() => auth.logOut()}>Logout</button>}
+                {auth.token && <>
+                    <div>
+                        <div className='flex-d align-center g-1'>
+                            <NavLink to="/admin">Home</NavLink>
+                            <NavLink to="/admin/astrophotography">Astrophotography</NavLink>
+                            <NavLink to="/admin/bookbindery">Bookbindery</NavLink>
+                            <button className='btn' onClick={() => auth.logOut()}>Logout</button>
+                        </div>  
+                    </div>
+                    
+                </>}
             </div>
         </header>
     )
--- client/src/layouts/Admin/EditPage.js
+++ client/src/layouts/Admin/EditPage.js
@@ -2,24 +2,55 @@ export default function EditPage() {
     return (
         <>
             <div className="container">
-                <div className="flex-d">
+                <div>
                     <h3>Home</h3>
                     <form>
-                        <input className="form-control" type="text" />
-                        <input className="form-control" type="text" />
-                        <input className="form-control" type="text" />
-                    </form>
-                </div>
-                <div>
-                    <h3>Astrophotography</h3>
-                    <form>
-                        <input type="text" />
-                    </form>
-                </div>
-                <div>
-                    <h3>Bookbindery</h3>
-                    <form>
-                        <input type="text" />
+                        <div className="home-section">
+                            <div className="form-group">
+                                <label htmlFor="hero-title">Hero Title</label>
+                                <input className="form-control" type="text" name="hero-title" placeholder="Hero Title" />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="hero-subtitle">Hero Subtitle</label>
+                                <textarea className="form-control" type="text" name="hero-subtitle" placeholder="Hero Subtitle" />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="hero-image" />
+                                <input className="form-control" type="file" name="hero-image" placeholder="Hero Image" accept="image/*" />
+                            </div>
+                        </div>
+                        
+                        <div className="astro-section">
+                            <div className="form-group">
+                                <label htmlFor="astro-title" />
+                                <input className="form-control" type="text" name="astro-title" placeholder="Astro Title" />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="astro-subtitle" />
+                                <textarea className="form-control" type="text" name="astro-subtitle" placeholder="Astro Subtitle" />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="astro-image" />
+                                <input className="form-control" type="file" name="astro-image" placeholder="Astro Image" accept="image/*" />
+                            </div>
+                        </div>
+                        
+                        <div className="bookbindery-section">
+                            <div className="form-group">
+                                <label htmlFor="bookbindery-title" />
+                                <input className="form-control" type="text" name="bookbindery-title" placeholder="Bookbindery Title" />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="bookbindery-subtitle" />
+                                <textarea className="form-control" type="text" name="bookbindery-subtitle" placeholder="Bookbindery Subtitle" />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="bookbindery-image" />
+                                <input className="form-control" type="file" name="bookbindery-image" placeholder="Bookbindery Image" accept="image/*" />
+                            </div>
+                        </div>
+                        
+                        <button className="btn">Update</button>
                     </form>
                 </div>
             </div>
